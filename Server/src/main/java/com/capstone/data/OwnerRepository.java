584ee3c58ffe8fb2f9b945a284245a4263df0228
Erik Klingenberg <personal.erik.k@gmail.com>
2024-06-19 22:16:25 -0700

    Owner model and repository.

---
+++ Server/src/main/java/com/capstone/data/OwnerRepository.java
@@ -0,0 +1,11 @@
+package com.capstone.data;
+
+import com.capstone.models.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+
+    @Query(value = "SELECT o FROM Owner o WHERE o.userName = ?1")
+    Owner getOwnerByUserName(String userName);
+}
---
+++ Server/src/main/java/com/capstone/models/Owner.java
@@ -0,0 +1,111 @@
+package com.capstone.models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long ownerId;
+
+    @Column(name = "first_name",
+            nullable = false,
+            columnDefinition = "text")
+    String firstName;
+
+    @Column(name = "last_name",
+            nullable = false,
+            columnDefinition = "text")
+    String lastName;
+
+    @Column(name = "email",
+            nullable = false,
+            columnDefinition = "text")
+    String email;
+
+    @Column(name = "user_name",
+            nullable = false,
+            columnDefinition = "text")
+    String userName;
+
+    @Column(name = "password",
+            nullable = false,
+            columnDefinition = "text")
+    String password;
+
+    public Owner() {
+    }
+
+    public Owner(String firstName, String lastName, String email, String userName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+    }
+
+
+    public long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Owner owner = (Owner) o;
+        return Objects.equals(getFirstName(), owner.getFirstName()) && Objects.equals(getLastName(), owner.getLastName()) && Objects.equals(getEmail(), owner.getEmail()) && Objects.equals(getUserName(), owner.getUserName()) && Objects.equals(getPassword(), owner.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getLastName(), getEmail(), getUserName(), getPassword());
+    }
+}

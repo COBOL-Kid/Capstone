710d3883ef6d7f60c5898a0088c070ea1f51ef0b
Erik Klingenberg <personal.erik.k@gmail.com>
2024-06-19 22:47:13 -0700

    reformatted and optimized imports

--- Server/src/main/java/com/capstone/data/VehicleInfoRepository.java
+++ Server/src/main/java/com/capstone/data/VehicleInfoRepository.java
@@ -2,9 +2,6 @@ package com.capstone.data;
 
 import com.capstone.models.VehicleInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.Optional;
 
 public interface VehicleInfoRepository extends JpaRepository<VehicleInfo, Long> {
 }

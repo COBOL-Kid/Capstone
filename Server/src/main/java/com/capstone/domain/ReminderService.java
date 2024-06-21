f0a3638211f024be7af292b733be7e975ecf54eb
Erik Klingenberg <personal.erik.k@gmail.com>
2024-06-21 05:19:10 -0700

    Update Reminder Service Method + Tests.

--- Server/src/main/java/com/capstone/domain/ReminderService.java
+++ Server/src/main/java/com/capstone/domain/ReminderService.java
@@ -4,6 +4,7 @@ import com.capstone.data.MaintenanceRecordRepositoryJPA;
 import com.capstone.data.ReminderRepositoryJPA;
 import com.capstone.models.Reminder;
 import com.capstone.models.Result;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.orm.jpa.JpaSystemException;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReminderService {
@@ -28,17 +30,48 @@ public class ReminderService {
         return reminderRepository.getRemindersByVinIdMatches(vinId);
     }
 
+    @Transactional
     public Result<Reminder> createReminder(Reminder reminder) {
         Result<Reminder> result = validateReminder(reminder);
         if (!result.isSuccess()) {
             return result;
         }
-        reminderInFuture(result);
+        result = reminderInFuture(reminder);
         if (!result.isSuccess()) {
             return result;
         }
         try {
             result.setPayload(reminderRepository.save(reminder));
+        } catch (DataIntegrityViolationException e) {
+            result.addError("DataIntegrityViolationException");
+        } catch (JpaSystemException e) {
+            result.addError("JpaSystemException");
+        }
+        return result;
+    }
+
+    @Transactional
+    public Result<Reminder> updateReminder(Reminder incomingReminder) {
+        Result<Reminder> result = validateReminder(incomingReminder);
+        if (!result.isSuccess()) {
+            return result;
+        }
+        result = reminderInFuture(incomingReminder);
+        if (!result.isSuccess()) {
+            return result;
+        }
+        Optional<Reminder> existingReminderOpt = reminderRepository.findById(incomingReminder.getReminderId());
+        if (existingReminderOpt.isEmpty()) {
+            result.addError("Reminder not found");
+            return result;
+        }
+        Reminder existingReminder = existingReminderOpt.get();
+        existingReminder.setVinId(incomingReminder.getVinId());
+        existingReminder.setMaintenanceRecordId(incomingReminder.getMaintenanceRecordId());
+        existingReminder.setDescription(incomingReminder.getDescription());
+        existingReminder.setReminderDate(incomingReminder.getReminderDate());
+        try {
+            result.setPayload(reminderRepository.save(existingReminder));
         } catch (DataIntegrityViolationException e) {
             result.addError("DataIntegrityViolationException");
         } catch (JpaSystemException e) {
@@ -57,13 +90,14 @@ public class ReminderService {
             result.addError("Reminder is invalid");
             return result;
         }
-        result.setPayload(reminder);
         return result;
     }
 
-    private void reminderInFuture(Result<Reminder> result) {
-        if (!result.getPayload().getReminderDate().isAfter(LocalDate.now())) {
+    private Result<Reminder> reminderInFuture(Reminder reminder) {
+        Result<Reminder> result = new Result<>();
+        if (!reminder.getReminderDate().isAfter(LocalDate.now())) {
             result.addError("Reminder can only be in the future");
         }
+        return result;
     }
 }
--- Server/src/test/java/com/capstone/domain/ReminderServiceTest.java
+++ Server/src/test/java/com/capstone/domain/ReminderServiceTest.java
@@ -12,8 +12,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -71,6 +73,46 @@ class ReminderServiceTest {
     void invalidCreateReminderBecauseDateIsNotInFutureTest() {
         Reminder reminder = new Reminder(1, 1, "description", LocalDate.now());
         Result<Reminder> result = reminderService.createReminder(reminder);
+        assertTrue(result.getErrors().contains("Reminder can only be in the future"));
+    }
+
+    @Test
+    void validUpdateReminderTest() {
+        Reminder oldReminder = new Reminder(1, 1, "description", LocalDate.of(2025, 8, 18));
+        Reminder newReminder = new Reminder(2, 2, "new description", LocalDate.of(2027, 8, 18));
+        when(reminderRepository.findById(any())).thenReturn(Optional.of(oldReminder));
+        when(reminderRepository.save(oldReminder)).thenReturn(newReminder);
+
+        Result<Reminder> result = reminderService.updateReminder(newReminder);
+
+        assertEquals(newReminder, result.getPayload());
+        assertTrue(result.isSuccess());
+    }
+
+    @Test
+    void invalidUpdateReminderBecauseReminderIsNullTest() {
+        Result<Reminder> result = reminderService.updateReminder(null);
+        assertTrue(result.getErrors().contains("Reminder is null"));
+    }
+
+    @Test
+    void invalidUpdateReminderBecauseReminderIsInvalidTest() {
+        Reminder newReminder = Mockito.mock(Reminder.class);
+        when(newReminder.isInvalid()).thenReturn(true);
+
+        Result<Reminder> result = reminderService.updateReminder(newReminder);
+
+        assertTrue(result.getErrors().contains("Reminder is invalid"));
+    }
+
+    @Test
+    void invalidUpdateReminderBecauseDateIsNotInFutureTest() {
+        Reminder newReminder = new Reminder(1, 1, "description", LocalDate.of(1999, 12, 12));
+
+        when(reminderRepository.getReferenceById(any())).thenReturn(newReminder);
+
+        Result<Reminder> result = reminderService.updateReminder(newReminder);
+
         assertTrue(result.getErrors().contains("Reminder can only be in the future"));
     }
 }

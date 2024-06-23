29856cf41a887dcf2ad5caebd689e86edd32ca04
Erik Klingenberg <personal.erik.k@gmail.com>
2024-06-22 23:42:50 -0700

    Updated DB schema UserEmail is now used as username, created JWT Config and Service, added jwt dependencies.

---
+++ Server/src/main/java/com/capstone/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,39 @@
+package com.capstone.configuration;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    //need to make AllArgsConstructor if add fields
+    private final JwtService jwtService;
+
+    @Autowired
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        //Authorization Header
+        final String authHeader = request.getHeader("Authorization");
+        final String jwtToken;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwtToken = authHeader.substring(7);
+        userEmail = jwtService.extractUserEmail(jwtToken);
+    }
+}

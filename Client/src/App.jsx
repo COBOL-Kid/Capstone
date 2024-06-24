fdfe9cdc52266639b032e0635a9ac07976ecdb38
Erik Klingenberg <personal.erik.k@gmail.com>
2024-06-24 08:53:41 -0700

    User Login and Signup Complete.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./Components/NavBar.jsx";
 import Landing from "./Pages/Landing.jsx";
 import React from "react";
 import AuthenticationForm from "./Components/AuthenticationForm.jsx";
+import {FleetOverview} from "./Pages/FleetOverview.jsx";
 
 function App() {
     const initialUser = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/" element={<Landing/>}/>
                 <Route path="/login" element={<AuthenticationForm setUser={setUser}/>}/>
                 <Route path="/signup" element={<AuthenticationForm setUser={setUser}/>}/>
+                <Route path="/fleet_overview" element={<FleetOverview/>}/>
             </Routes>
         </BrowserRouter>
     )

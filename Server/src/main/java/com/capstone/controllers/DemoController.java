828e0bb7dc5eb1fe6bf3973d8d41f6319581a030
Erik Klingenberg <personal.erik.k@gmail.com>
2024-06-23 23:48:30 -0700

    Fully working authorization

--- Server/src/main/java/com/capstone/controllers/DemoController.java
+++ Server/src/main/java/com/capstone/controllers/DemoController.java
@@ -2,9 +2,11 @@ package com.capstone.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/demo")
+@RestController
+@RequestMapping("/api/demo")
 public class DemoController {
 
     @GetMapping("/hello")

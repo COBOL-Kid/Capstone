c706929204617847b411b9e852c0ff73644e222d
Erik Klingenberg <personal.erik.k@gmail.com>
2024-06-27 00:10:36 -0700

    Changed Icon Image and Navbar Title

--- Client/src/Components/NavBar.jsx
+++ Client/src/Components/NavBar.jsx
@@ -7,10 +7,11 @@ import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import LogoIcon from '@mui/icons-material/Flare';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 export default function NavBar() {
     const [anchorEl, setAnchorEl] = React.useState(null);
+    const navigate = useNavigate()
 
     const handleMenu = (event) => {
         setAnchorEl(event.currentTarget);
@@ -24,10 +25,10 @@ export default function NavBar() {
         <AppBar position="static">
             <Toolbar>
                 <IconButton edge="start" color="inherit" aria-label="Logo">
-                    <LogoIcon />
+                    <img src={"/public/honestCar.png"}  alt="logo" style={{width: "40px", height: "40px"}}/>
                 </IconButton>
                 <Typography variant="h6" style={{ flexGrow: 1, textAlign: 'center' }}>
-                    Website Title
+                    Honest Car
                 </Typography>
                 <div>
                     <IconButton

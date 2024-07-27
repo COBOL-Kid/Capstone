a0ef7330f0191ed88e37e01fb923a78605e9902d
Erik Klingenberg <personal.erik.k@gmail.com>
2024-07-27 03:02:22 -0700

    Front End Formatting

--- Client/src/Components/Errors.jsx
+++ Client/src/Components/Errors.jsx
@@ -1,36 +1,46 @@
-import {Dialog, DialogContent, DialogTitle, List, ListItem} from '@mui/material';
-import * as React from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  List,
+  ListItem,
+} from "@mui/material";
+import * as React from "react";
 
-export const Errors = ({errors}) => {
-    const [open, setOpen] = React.useState(false);
+export const Errors = ({ errors }) => {
+  const [open, setOpen] = React.useState(false);
 
-    React.useEffect(() => {
-        if (errors && errors.length > 0) {
-            setOpen(true);
-        } else {
-            setOpen(false);
-        }
-    }, [errors]);
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    if (!errors || errors.length === 0) {
-        return null;
+  React.useEffect(() => {
+    if (errors && errors.length > 0) {
+      setOpen(true);
+    } else {
+      setOpen(false);
     }
+  }, [errors]);
 
-    return (
-        <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description">
-            <DialogTitle id="alert-dialog-title">{"An Error Occurred"}</DialogTitle>
-            <DialogContent>
-                <List>
-                    {errors.map((error, index) => (
-                        <ListItem key={index}>{error}</ListItem>
-                    ))}
-                </List>
-            </DialogContent>
-        </Dialog>
-    );
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  if (!errors || errors.length === 0) {
+    return null;
+  }
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{"An Error Occurred"}</DialogTitle>
+      <DialogContent>
+        <List>
+          {errors.map((error, index) => (
+            <ListItem key={index}>{error}</ListItem>
+          ))}
+        </List>
+      </DialogContent>
+    </Dialog>
+  );
 };
--- Client/src/Components/VehicleCards.jsx
+++ Client/src/Components/VehicleCards.jsx
@@ -1,45 +1,49 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import {useNavigate} from "react-router-dom";
-import {Box} from "@mui/system";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
+import { Box } from "@mui/system";
 
-export default function VehicleCards({vehicles, setChosenVehicle}) {
+export default function VehicleCards({ vehicles, setChosenVehicle }) {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    return (
-        <Box sx={{display: 'flex', flexWrap: 'wrap', gap: '16px'}}>
-            {vehicles.map((vehicle) => (
-                <Card sx={{width: '350px'}} key={vehicle.vinId}>
-                    <CardMedia
-                        sx={{height: 170}}
-                        image={vehicle.image}
-                        title={vehicle.model}
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {vehicle.make} {vehicle.model}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {vehicle.vin}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Mileage: {vehicle.mileage}
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="large" onClick={() => {
-                            setChosenVehicle(vehicle);
-                            navigate("/vehicle_overview")
-                        }}>See Details</Button>
-                    </CardActions>
-                </Card>
-            ))}
-        </Box>
-    );
+  return (
+    <Box sx={{ display: "flex", flexWrap: "wrap", gap: "16px" }}>
+      {vehicles.map((vehicle) => (
+        <Card sx={{ width: "350px" }} key={vehicle.vinId}>
+          <CardMedia
+            sx={{ height: 170 }}
+            image={vehicle.image}
+            title={vehicle.model}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {vehicle.make} {vehicle.model}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {vehicle.vin}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Mileage: {vehicle.mileage}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              size="large"
+              onClick={() => {
+                setChosenVehicle(vehicle);
+                navigate("/vehicle_overview");
+              }}
+            >
+              See Details
+            </Button>
+          </CardActions>
+        </Card>
+      ))}
+    </Box>
+  );
 }

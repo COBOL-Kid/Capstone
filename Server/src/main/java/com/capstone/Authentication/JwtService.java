20c1ec6a93b9dc7201e0682bdcc93a8a33e49f53
Erik Klingenberg <166357941+Sholzby@users.noreply.github.com>
2024-10-29 00:00:17 -0500

    Revert "Renamed Authentication Folder."

--- Server/src/main/java/com/capstone/Authentication/JwtService.java
+++ Server/src/main/java/com/capstone/Authentication/JwtService.java
@@ -1,4 +1,4 @@
-package com.capstone.authentication;
+package com.capstone.Authentication;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
--- Server/src/main/java/com/capstone/configuration/SecurityConfig.java
+++ Server/src/main/java/com/capstone/configuration/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.capstone.authentication.JwtAuthenticationFilter;
+import com.capstone.Authentication.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity

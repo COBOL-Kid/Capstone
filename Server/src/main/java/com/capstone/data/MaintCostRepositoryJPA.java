a73a089a9ed5f9e5b8195e58b0c3561c4823ccad
Erik Klingenberg <klingenberg.e@icloud.com>
2026-05-05 23:56:04 +0000

    Add endpoint for retrieving maintenance costs

--- Server/src/main/java/com/capstone/data/MaintCostRepositoryJPA.java
+++ Server/src/main/java/com/capstone/data/MaintCostRepositoryJPA.java
@@ -3,5 +3,9 @@ package com.capstone.data;
 import com.capstone.models.MaintCost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MaintCostRepositoryJPA extends JpaRepository<MaintCost, Long> {
+    
+    List<MaintCost> findByVehicleTypeId_VehicleTypeId(Long vehicleTypeId);
 }

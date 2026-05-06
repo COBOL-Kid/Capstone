99d1993be370f54ef433a2a69f7186f8c13394a2
Erik Klingenberg <klingenberg.e@icloud.com>
2026-05-06 23:22:35 +0000

    Update layout, typography, and add new Angular pages

---
+++ Client/src/app/pages/about-page/about-page.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-page',
+  templateUrl: './about-page.html',
+  styleUrl: './about-page.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutPageComponent {}
